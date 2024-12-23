30bb88767b0d911358a08859464e0b349bcb0eff
griffin-director <ros2griffin@gmail.com>
2024-12-23 23:08:04 +0900

    Add Install All Node Packages
    Add Install All Node Packages

---
+++ apt_install_all_nodes.sh
@@ -0,0 +1,7 @@
+# mongodb gpg 추가
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/mongodb-server-6.0.asc
+# mongodb 저장소 추가
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt update
+sudo xargs apt -y install < all_nodes_packages.txt
